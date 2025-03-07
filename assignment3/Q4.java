1a03cae22a677ccdd3a85913410f45d3d2a4d566
Abhipsa Mohapatra <166213669+AbhipsaMohapatra@users.noreply.github.com>
2025-03-07 11:03:48 +0530

    Update Q4.java

--- assignment3/Q4.java
+++ assignment3/Q4.java
@@ -37,6 +37,16 @@ class Student{
 		this.age = age;
 		this.mark = mark;
 	}
+	  @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Student student = (Student) obj;
+        return age == student.age &&
+                mark == student.mark &&
+                name.equals(student.name);
+    }
+
 	
 	
 }
